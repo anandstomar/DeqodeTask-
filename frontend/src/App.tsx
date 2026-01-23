61d8b941e1ea146322e2c27398452b612be361e2
anandstomar <anandsinghtomar25.com>
2026-01-23 20:04:51 +0530

    Add missing files auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import LoginPage from "./components/auth/loginpage";
-import RegisterPage from "./components/auth/registerpage";
+import LoginPage from "./components/auth/LoginPages";
+import RegisterPage from "./components/auth/RegisterPages";
 import ChatLayout from "./components/chat/chatlayout";
 
 const queryClient = new QueryClient();
